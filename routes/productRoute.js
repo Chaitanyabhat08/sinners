7c7ec87736a7a8d9c2e5d99518d140dd3e99ad04
samriddhi <samriddhi@edusmart.com>
2023-05-08 13:22:49 +0530

    route fix

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,9 +3,7 @@ const express = require("express");
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails,createProductReviews,getAllReviews,deleteReviews , getProductsCategoryWise} = require("../controllers/product");
 const { isAuthenticatedUser , authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
-
-router.get('/products/getallproducts', getAllProducts);
-router.get('/products/categoryWise/:category', getProductsCategoryWise);
+router.get('/api/v1/products/categoryWise/:category', getProductsCategoryWise);
 router.post('/admin/products/createNewProduct', isAuthenticatedUser,authorizeRoles("admin"),createProduct);
 router.get('/products/getProductDetails/:id', getProductDetails);
 router.put('/admin/products/updateProduct/:id',isAuthenticatedUser,authorizeRoles("admin"), updateProduct);
