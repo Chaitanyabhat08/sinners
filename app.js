791585c1a4a2da5c5b06da3bb1c39fc466104a41
samriddhi <samriddhi@edusmart.com>
2023-06-04 13:15:19 +0530

    razorpay gateway

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
 dotenv.config({ path: '../config/.env' });
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(cors());
 //route imports
 const product = require('./routes/productRoute');
 const user = require('./routes/userRoute');
 const order = require('./routes/orderRoute');
-const payment = require('./routes/paymentRoute');
 
 app.use("/api/v1/", product);
 app.use("/api/v1/", user);
 app.use("/api/v1/", order);
-app.use("/api/v1/", payment);
 
 //middleware for error
 app.use(errorMiddleware);
--- server.js
+++ server.js
@@ -1,11 +1,48 @@
-const app = require('./app');
-//config
+const express = require('express');
+const app = express();
 const cloudinary = require('cloudinary');
 const dotenv = require('dotenv');
-const connect = require('./config/database');
 const connectDatabase = require('./config/database');
+const Razorpay = require('razorpay');
+const shortid = require('shortid');
+const path = require('path');
 
-//Handling uncaught exceptions
+dotenv.config({ path: './config/.env' });
+
+const razorpay = new Razorpay({
+    key_id: "rzp_test_wBU9x1cN57zN3f",
+    key_secret: "uci7KIIDxHSlSJ62MlNaB1GB",
+});
+
+app.get('/logo.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, 'logo.jpg'));
+});
+
+app.post('/razorpay', async (req, res) => {
+    const payment_capture = 1;
+    const amount = 499;
+    const currency = 'INR';
+    const options = {
+        amount: amount * 100,
+        currency: currency,
+        receipt: shortid.generate(),
+        payment_capture
+    };
+
+    try {
+        const response = await razorpay.orders.create(options);
+        console.log('ress',response);
+        res.json({
+            id: response.id,
+            currency: response.currency,
+            amount: response.amount,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Handling uncaught exceptions
 process.on('uncaughtException', err => {
     console.log("Error: " + err.message);
     console.log('Shutting down server due to uncaught Exception: ' + err.stack);
@@ -13,19 +50,20 @@ process.on('uncaughtException', err => {
         process.exit(1);
     });
 });
-dotenv.config({ path: './config/.env' });
 
 connectDatabase();
+
 const server = app.listen(process.env.PORT, () => {
-    console.log(`your server is running on http://localhost:${process.env.PORT}`)
+    console.log(`Your server is running on http://localhost:${process.env.PORT}`);
 });
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-})
-//unhandled promise rejection
+});
+
+// Unhandled promise rejection
 process.on('unhandledRejection', err => {
     console.log("Error: " + err.message);
     console.log('Shutting down server due to unhandled rejection: ' + err.stack);
